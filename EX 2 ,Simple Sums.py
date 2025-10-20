308339318dd91203d2bd1da6f77248aa6e8a80f6
Eshan <noteshan6927@gmail.com>
2025-10-20 16:18:34 +0400

    Add files via upload

---
+++ EX 2 ,Simple Sums.py
@@ -0,0 +1,9 @@
+"""
+ASSESSMENT 1
+EX 2
+Simple Sums
+"""
+num1=8 #num1 is the variable name and has the integer value of 8 assigned to it
+num2=10 #num2 is the variable name and has the integer value of 10 assigned to it
+sum=num1+num2 #sum is the variable that stores the sum of the first 2 numbers(variables)
+print(sum)#shows the output of sum
