279816bf8241658a69097fbfb548f8ed04cdd107
Eshan <noteshan6927@gmail.com>
2025-10-20 16:15:34 +0400

    Add files via upload

---
+++ EX1 ,Coding is Cool.py
@@ -0,0 +1,9 @@
+"""
+ASSESSMENT 1
+EX 1
+Coding is Cool
+"""
+word1 ="Coding"#word1 is a variable to store a value(Coding)
+word2 ="is"#word2 is a variable to store a value(is)
+word3 ="Cool"#word3 is a variable to store a value(Cool)
+print(word1 + " " +word2 +" " +word3)#shows the 3 variables combined to form a sentence
