cc78122d9c10aade18994301b8d75338ad25e380
Eshan <noteshan6927@gmail.com>
2025-10-24 17:25:50 +0400

    Add files via upload

---
+++ EX 4,Primitive Quiz With Advanced Requirements.py
@@ -0,0 +1,65 @@
+
+"""
+EX 4 with Advanced Requirements
+Primitive Quiz
+ASSESSMENT 1 
+"""
+fra=input("What is the capital of France?")#allows you to input what is the capital of paris?
+if fra.lower() == "paris":#if condition is used to check if the answer is correct .lower() is used so that answer will be outputed as correct regardless of capitilization
+    print("Answer is correct")#shows if the answer is correct
+else:#if answer is wrong it goes to the else condition
+    print("Answer is wrong")#displays that the answer is wrong 
+    
+ger=input("What is the capital of Germany?")#allows you to input what is the capital of germany?    
+if ger.lower() == "berlin":#if condition is used to check if the answer is correct .lower() is used so that answer will be outputed as correct regardless of capitilization
+    print("Answer is correct")#shows if the answer is correct
+else:#if answer is wrong it goes to the else condition
+    print("Answer is wrong")#displays that the answer is wrong 
+
+por=input("What is the capital of Portugal?")#allows you to input what is the capital of portugal?    
+if por.lower() == "lisbon":#if condition is used to check if the answer is correct .lower() is used so that answer will be outputed as correct regardless of capitilization
+    print("Answer is correct")#shows if the answer is correct
+else:#if answer is wrong it goes to the else condition
+    print("Answer is wrong")#displays that the answer is wrong     
+    
+spa=input("What is the capital of Spain?")  #allows you to input what is the capital of spain?   
+if spa.lower() == "madrid":#if condition is used to check if the answer is correct .lower() is used so that answer will be outputed as correct regardless of capitilization
+    print("Answer is correct")#shows if the answer is correct
+else:#if answer is wrong it goes to the else condition
+    print("Answer is wrong")#displays that the answer is wrong    
+    
+bel=input("What is the capital of Belgium?")     #allows you to input what is the capital of belgium?  
+if bel.lower() == "brussels":#if condition is used to check if the answer is correct .lower() is used so that answer will be outputed as correct regardless of capitilization
+    print("Answer is correct")#shows if the answer is correct
+else: #if answer is wrong it goes to the else condition   
+    print("Answer is wrong")#displays that the answer is wrong    
+
+ita=input("What is the capital of Italy?")     #allows you to input what is the capital of italy?  
+if ita.lower() == "rome":#if condition is used to check if the answer is correct .lower() is used so that answer will be outputed as correct regardless of capitilization
+    print("Answer is correct")#shows if the answer is correct
+else:#if answer is wrong it goes to the else condition
+    print("Answer is wrong")#displays that the answer is wrong  
+    
+ned=input("What is the capital of Netherlands?")     #allows you to input what is the capital of netherlands?  
+if ned.lower() == "amsterdam":#if condition is used to check if the answer is correct .lower() is used so that answer will be outputed as correct regardless of capitilization
+    print("Answer is correct")#shows if the answer is correct
+else:#if answer is wrong it goes to the else condition
+    print("Answer is wrong")#displays that the answer is wrong  
+    
+gre=input("What is the capital of Greece?")     #allows you to input what is the capital of greece?  
+if gre.lower() == "athens":#if condition is used to check if the answer is correct .lower() is used so that answer will be outputed as correct regardless of capitilization
+    print("Answer is correct")#shows if the answer is correct
+else:#if answer is wrong it goes to the else condition
+    print("Answer is wrong")#displays that the answer is wrong     
+    
+cro=input("What is the capital of Croatia?")     #allows you to input what is the capital of croatia?  
+if cro.lower() == "zagreb":#if condition is used to check if the answer is correct .lower() is used so that answer will be outputed as correct regardless of capitilization
+    print("Answer is correct")#shows if the answer is correct
+else:#if answer is wrong it goes to the else condition
+    print("Answer is wrong")#displays that the answer is wrong  
+    
+swe=input("What is the capital of Sweden?")     #allows you to input what is the capital of sweden?  
+if swe.lower() == "stockholm":#if condition is used to check if the answer is correct .lower() is used so that answer will be outputed as correct regardless of capitilization
+    print("Answer is correct")#shows if the answer is correct
+else:#if answer is wrong it goes to the else condition
+    print("Answer is wrong")#displays that the answer is wrong      
