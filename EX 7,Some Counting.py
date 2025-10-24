6c6d1636113c598051ddf030ea9c5573f652ce51
Eshan <noteshan6927@gmail.com>
2025-10-24 17:31:30 +0400

    Add files via upload

---
+++ EX 7,Some Counting.py
@@ -0,0 +1,25 @@
+
+"""
+Ex 7
+ASSESSMENT 1
+Some Counting
+"""
+print ("loop1")#shows the start of loop 1
+for i in range(51):#for loop used for numbers from 0 to 50 in increments of 1
+    print(i)#prints the for loop 
+
+print("loop2")#shows the start of loop 2
+for a in range(50, -1, -1):#for loop used for numbers from 50 to 0 in decrements of -1
+    print(a)#prints the for loop 
+
+print("loop3")#shows the start of loop 3
+for b in range(30,51,1):#for loop used for numbers from 30 to 50 in increments of 1
+    print(b)#prints the for loop 
+
+print("loop4")#shows the start of loop 4
+for c in range(50,9,-2):#for loop used for numbers from 50 to 10 in decrements of -2
+    print(c)#prints the for loop 
+
+print("loop5")#shows the start of loop 5
+for d in range(100,201,5):#for loop used for numbers from 100 to 200 in increments of 5
+    print(d)#prints the for loop 
