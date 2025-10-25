6d51b1c23caa1444d21613ee74e42affceeed2ab
Eshan <noteshan6927@gmail.com>
2025-10-25 15:10:02 +0400

    Add files via upload

---
+++ EX 9,Hello.py
@@ -0,0 +1,14 @@
+
+"""
+Exercise 9
+Hello
+ASSESSMENT 1
+"""
+def hello():#defines the hello function
+    print("Hello")  # Prints "Hello" to the console
+
+def main():#defined the main function
+    hello()  # Calls the hello() function
+
+if __name__ == "__main__":
+    main()#calls the main function
